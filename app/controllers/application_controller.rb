5d2b965be2e0274bbec85ea1a210f3d948023b6a
ytung <ytung@mit.edu>
2013-11-16 23:28:48 -0500

    started application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	def new
+		@application = Application.new
+	end
+
+	def create
+		@application = Application.new(application_params)
+		#@application.posting = #current posting
+		response_to do |format|
+			if @menu.save
+				format.html {}
+			else
+			end
+	end
 
 end
--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,6 @@
 class Application < ActiveRecord::Base
+	belongs_to :student
+	belongs_to :posting
+
+	
 end
