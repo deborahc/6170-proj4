14cdd9e6c46da6ef53bb555c065a1b0fd997d62a
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-11-16 22:26:41 -0500

    Adding login/logout with password authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
+
+	# def self.authenticate(email, input_password_digest)
+	# 	logger.info input_password_digest
+	# 	logger.info 'blah'
+	# 	user = find_by_email(email)
+	# 	if user && (user.password_digest == input_password_digest)
+	# 		user
+	# 	else
+	# 		nil
+	# 	end
+	# end
+
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,12 @@ NlujanDeborahcYtungDbgeorgeFinal::Application.routes.draw do
 
   resources :students, :controller => 'users', :type => 'Student'
   resources :supervisors, :controller => 'users', :type => 'Supervisor'
+  resources :sessions
+
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
