e64039a15703f1927eff053c2a9407f7321ad510
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-11-18 00:48:43 -0500

    Validations for posting and urop applications

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,7 +1,15 @@
 class Posting < ActiveRecord::Base
 	has_many :urop_applications
 	belongs_to :supervisor
+	
+	validates :title, :presence => true
 
+	validates :description, :presence => true
+	validates :skills, :presence => true
+	validates :funding_type, :presence => true
+
+
+	# Method to allow searching by description on all postings page
 	def self.search(search)
   		if search
     		where('description LIKE ?', "%#{search}%")
--- app/models/urop_application.rb
+++ app/models/urop_application.rb
@@ -2,4 +2,6 @@ class UropApplication < ActiveRecord::Base
 	belongs_to :student
 	belongs_to :posting
 	has_many :supervisors, through: :postings
+
+
 end
