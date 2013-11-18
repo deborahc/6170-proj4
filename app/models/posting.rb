7cb5899f88a56723d2df7c04c00dc003fb631ee1
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-11-17 19:01:30 -0500

    Adding supervisor has many applications through postings relationship

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -1,5 +1,4 @@
 class Posting < ActiveRecord::Base
 	has_many :urop_applications
-
 	belongs_to :supervisor
 end
--- app/models/supervisor.rb
+++ app/models/supervisor.rb
@@ -1,3 +1,4 @@
 class Supervisor < User
 	has_many :postings
+	has_many :applications, through: :postings
 end
