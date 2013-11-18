710c117216df75f3052f5f61de3655a1de7e62db
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-11-17 22:26:49 -0500

    Making postings page

--- app/controllers/postings_controller.rb
+++ app/controllers/postings_controller.rb
@@ -69,6 +69,6 @@ class PostingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def posting_params
-      params.require(:posting).permit(:title, :description, :skills, :funding_type)
+      params.require(:posting).permit(:title, :description, :skills, :funding_type, :supervisor_id)
     end
 end
