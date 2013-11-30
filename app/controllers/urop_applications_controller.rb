04a6ed727c7b8f3337a3f12e79d74550a24ffde5
ytung <ytung@mit.edu>
2013-11-29 20:40:23 -0500

    working on sending emails between supervisors and students

--- app/controllers/urop_applications_controller.rb
+++ app/controllers/urop_applications_controller.rb
@@ -1,5 +1,5 @@
 class UropApplicationsController < ApplicationController
-
+	before_action :set_urop_application, only: [:destroy]
 	before_action :can_create, only: [:new, :create]
 
 	def index
@@ -24,11 +24,23 @@ class UropApplicationsController < ApplicationController
 		@urop_application = UropApplication.new(application_params)
 		respond_to do |format|
 			if @urop_application.save
+				#Send a copy of the message to the supervisor's email address
+				@message = application_params[:message]
+				@title = @urop_application.posting.title
+				UserMailer.send_request(current_user,@urop_application.posting.supervisor,@message,@title)
 				format.html { redirect_to postings_path }
 			else
 				format.html { redirect_to postings_path }
 			end
 		end
+	end
+
+	def destroy
+		@urop_application.destroy
+		respond_to do |format|
+			format.html { redirect_to urop_applications_path }
+			format.json { head :no_content }
+	    end
 	end
 
 	def supervisor_application_index
@@ -40,10 +52,23 @@ class UropApplicationsController < ApplicationController
 		@urop_applications = current_user.urop_applications
 	end
 
+	def email
+		@application = UropApplication.find(params[:application])
+		@title = @application.posting.title
+		@message = params['message']
+		puts "hello"
+		puts @message
+		UserMailer.send_reply(current_user,@application.student,@title,@message)
+	end
+
 	private
 
+	def set_urop_application
+      @urop_application = UropApplication.find(params[:id])
+    end
+
 	def application_params
-		params.require(:urop_application).permit(:message, :supervisor_id, :posting_id, :student_id)
+		params.require(:urop_application).permit(:message, :supervisor_id, :posting_id, :student_id, :status)
 	end
 
     # Only users can create applications
