5aaae39ffebd047d0d6e918dfef14b578d1e7f39
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-12-07 15:06:19 -0500

    Removing line from user.rb for heroku test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	has_attached_file :resume,
-	:storage => :s3,
-	:s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
-    # Authenticated read prevents access to resumes by changing the URL
-    # Authentication to s3 file expires after 1 hour
-    :s3_permissions => :authenticated_read
+	has_attached_file :resume
+	# :storage => :s3,
+	# :s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
+ #    # Authenticated read prevents access to resumes by changing the URL
+ #    # Authentication to s3 file expires after 1 hour
+ #    :s3_permissions => :authenticated_read
     
 	validates :first_name, :presence => true
 	validates :last_name, :presence => true
