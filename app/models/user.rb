0855e342ec418a1a1043238bf26c13163dd203cc
Deborah <deborahc@ubuntu.ubuntu-domain>
2013-12-09 00:36:09 -0500

    S3 edit to push to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	#has_attached_file :resume
+	has_attached_file :resume
 
-	#Uncomment to run locally
-	has_attached_file :resume,
-	:storage => :s3,
-	:s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
- 	# Authenticated read prevents access to resumes by changing the URL
- 	# Authentication to s3 file expires after 1 hour
- 	:s3_permissions => :authenticated_read
+	# #Uncomment to run locally
+	# has_attached_file :resume,
+	# :storage => :s3,
+	# :s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
+ # 	# Authenticated read prevents access to resumes by changing the URL
+ # 	# Authentication to s3 file expires after 1 hour
+ # 	:s3_permissions => :authenticated_read
  	
 	validate :resume_is_pdf
 	validate :resume_is_smaller_than_four_megabytes
